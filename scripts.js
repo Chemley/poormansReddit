fb887265afe361af4da7e4c399a7bc095ca37ddd
Chemley <canaowens@gmail.com>
2017-11-06 19:19:37 -0500

    got it to work. Reworked the JS to make the div and content appear on page.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
   console.log("ready!");
   // get the json informaiton from the reddit page.
-  $.get("https://www.reddit.com/r/aww/.json", funciton(data) {
-    data.forEach(function(data) {
-      for (i = 1; i < 10; i++) {
-        var data = data[i];
+  $.get("https://www.reddit.com/r/aww/.json", function(data) {
+    var image = data.data.childern.data.preview.images.source.url;
+    var text = data.childern.data.title;
+    var url = data.childern.data.url;
+    var author = data.childern.data.author;
 
         // Create the post.
-        var postEl = $("<section class= 'post'>");
+        var postEl = $("<div class= 'post'>");
 
         // Create the image which is inside of a second array. Must loop through that array to get the image each time.
-        data.data.data.childern[0].data.preview.images[0].source.url.forEach(function(image) {
+        .forEach(function(image) {
           var imageEl = $("<img class= 'image'>");
           imageEl.append(image);
           postEl.append(imageEl);
@@ -22,21 +23,21 @@ $(document).ready(function() {
 
         // Create the title as a link.
         var titleEl = $("<a class= 'title'>");
-        titleEl.text(data.data.childern[0].data.title);
+        titleEl.text(text);
         textEl.append(titleEl);
 
         // Create the URL
         var urlEl = $("<a class= 'url'>");
-        urlEl.text(data.childern.data.url);
+        urlEl.text(url);
         textEl.append(urlEl);
 
         //display the author
         var authorEl = $("<p class= 'author'>");
-        authorEl.text(data.data.childern.data.author);
+        authorEl.text(author);
         textEl.append(authorEl);
 
         // Append the post to the main container with id "main".
-        ("#main").append(postEl);
+        $("#main").append(postEl);
       }
     });
   });
